1666d5e57a0d29ee3b5bb073a2f1cc805cf9d0f3
linuxing3 <linuxing3@qq.com>
2025-07-02 14:35:13 -0300

    refactor: dynamically generate age secrets from directory contents

--- modules/nixos/security.nix
+++ modules/nixos/security.nix
@@ -2,10 +2,17 @@
   pkgs,
   config,
   flake,
+  lib,
   ...
 }: let
   inherit (flake) inputs;
   inherit (inputs) mysecrets;
+  mapListToAttrs = m: f:
+    lib.listToAttrs (map (name: {
+        inherit name;
+        value = f name;
+      })
+      m);
   user_access = {
     owner = "linuxing3";
     mode = "0500";
@@ -18,6 +25,10 @@
     owner = "root";
     mode = "0000";
   };
+  dirContents = builtins.readDir "${inputs.mysecrets}";
+  fileNames = builtins.attrNames dirContents;
+  regularFiles = builtins.filter (name: dirContents.${name} == "regular") fileNames; # Filters for regular files
+  baseNames = map (name: builtins.replaceStrings [".age"] [""] name) regularFiles; # Removes .nix extension
 in {
   imports = [
     inputs.sops-nix.nixosModules.sops
@@ -65,33 +76,39 @@ in {
     "/etc/ssh/ssh_host_ed25519_key"
   ];
 
-  age.secrets."generic" = {
-    file = "${mysecrets}/nix-access-tokens.age";
-    owner = "linuxing3";
-  };
+  age.secrets = mapListToAttrs baseNames (
+    name: {
+      file = "${mysecrets}/${name}.age";
+      owner = "linuxing3";
+    }
+  );
+  # age.secrets."generic" = {
+  #   file = "${mysecrets}/nix-access-tokens.age";
+  #   owner = "linuxing3";
+  # };
 
-  age.secrets."username" = {
-    file = "${mysecrets}/username.age";
-    owner = "linuxing3";
-  };
+  # age.secrets."username" = {
+  #   file = "${mysecrets}/username.age";
+  #   owner = "linuxing3";
+  # };
 
-  age.secrets."fullname" = {
-    file = "${mysecrets}/fullname.age";
-    owner = "linuxing3";
-  };
+  # age.secrets."fullname" = {
+  #   file = "${mysecrets}/fullname.age";
+  #   owner = "linuxing3";
+  # };
 
-  age.secrets."cachix-auth-token" = {
-    file = "${mysecrets}/cachix-auth-token.age";
-    owner = "linuxing3";
-  };
-  age.secrets."deepseek-token" = {
-    file = "${mysecrets}/deepseek-token.age";
-    owner = "linuxing3";
-  };
-  age.secrets."gemini-token" = {
-    file = "${mysecrets}/gemini-token.age";
-    owner = "linuxing3";
-  };
+  # age.secrets."cachix-auth-token" = {
+  #   file = "${mysecrets}/cachix-auth-token.age";
+  #   owner = "linuxing3";
+  # };
+  # age.secrets."deepseek-token" = {
+  #   file = "${mysecrets}/deepseek-token.age";
+  #   owner = "linuxing3";
+  # };
+  # age.secrets."gemini-token" = {
+  #   file = "${mysecrets}/gemini-token.age";
+  #   owner = "linuxing3";
+  # };
 
   environment.sessionVariables = {
     "DEEPSEEK_API_KEY" = ''
